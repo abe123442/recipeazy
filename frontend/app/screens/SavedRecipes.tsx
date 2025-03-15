85922878f938336dec7ba3231fe16fe763adf9d1
camellionn <thanhtra2004@gmail.com>
2025-03-16 02:35:31 +1100

    Make changes to the RecipeCard component

--- frontend/app/screens/SavedRecipes.tsx
+++ frontend/app/screens/SavedRecipes.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { View } from "react-native";
 import { Heading } from "@/components/ui/heading";
+import RecipeCard from "../components/recipeCard";
+
 const SavedRecipes = () => {
   return (
     <View>
       <Heading>Saved Recipes</Heading>
+      <RecipeCard />
     </View>
   );
 };
