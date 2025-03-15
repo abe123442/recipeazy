6d314e3146dee280451299e1174fb87a12842f8f
camellionn <thanhtra2004@gmail.com>
2025-03-16 02:12:42 +1100

    Flesh out the HomePage

--- frontend/app/components/dropDownFilter.tsx
+++ frontend/app/components/dropDownFilter.tsx
@@ -3,9 +3,11 @@ import { ChevronDownIcon } from "@/components/ui/icon"
 	
 function DropDownFilter() {
   return (
-    <Select>
-          <SelectTrigger variant="outline" size="md" >
-            <SelectInput placeholder="Select option" />
+    <div className='flex flex-col gap-y-4 justify-center pt-3 p-5'>
+    {/* Kitchen Staples */}
+    <Select className='w-auto'>
+          <SelectTrigger variant="outline" size="xl" >
+            <SelectInput placeholder="Kitchen Staples" />
             <SelectIcon className="mr-3" as={ChevronDownIcon} />
           </SelectTrigger>
           <SelectPortal>
@@ -14,24 +16,114 @@ function DropDownFilter() {
               <SelectDragIndicatorWrapper>
                 <SelectDragIndicator />
               </SelectDragIndicatorWrapper>
-              <SelectItem label="UX Research" value="ux" />
-              <SelectItem label="Web Development" value="web" />
+              <SelectItem label="1" value="1" />
               <SelectItem
-                label="Cross Platform Development Process"
-                value="Cross Platform Development Process"
+                label="2"
+                value="2"
               />
               <SelectItem
-                label="UI Designing"
-                value="ui"
+                label="3"
+                value="3"
                 isDisabled={true}
               />
               <SelectItem
-                label="Backend Development"
-                value="backend"
+                label="4"
+                value="4"
               />
             </SelectContent>
           </SelectPortal>
         </Select>
+
+    {/* Vegetables & Fruits */}
+    <Select className='w-auto'>
+          <SelectTrigger variant="outline" size="xl" >
+            <SelectInput placeholder="Vegetables & Fruits" />
+            <SelectIcon className="mr-3" as={ChevronDownIcon} />
+          </SelectTrigger>
+          <SelectPortal>
+            <SelectBackdrop/>
+            <SelectContent>
+              <SelectDragIndicatorWrapper>
+                <SelectDragIndicator />
+              </SelectDragIndicatorWrapper>
+              <SelectItem label="1" value="1" />
+              <SelectItem
+                label="2"
+                value="2"
+              />
+              <SelectItem
+                label="3"
+                value="3"
+                isDisabled={true}
+              />
+              <SelectItem
+                label="4"
+                value="4"
+              />
+            </SelectContent>
+          </SelectPortal>
+        </Select>
+
+    {/* Dairy */}
+    <Select className='w-auto'>
+          <SelectTrigger variant="outline" size="xl" >
+            <SelectInput placeholder="Dairy" />
+            <SelectIcon className="mr-3" as={ChevronDownIcon} />
+          </SelectTrigger>
+          <SelectPortal>
+            <SelectBackdrop/>
+            <SelectContent>
+              <SelectDragIndicatorWrapper>
+                <SelectDragIndicator />
+              </SelectDragIndicatorWrapper>
+              <SelectItem label="1" value="1" />
+              <SelectItem
+                label="2"
+                value="2"
+              />
+              <SelectItem
+                label="3"
+                value="3"
+                isDisabled={true}
+              />
+              <SelectItem
+                label="4"
+                value="4"
+              />
+            </SelectContent>
+          </SelectPortal>
+        </Select>
+    
+    {/* Meats */}
+    <Select className='w-auto'>
+          <SelectTrigger variant="outline" size="xl" >
+            <SelectInput placeholder="Meats" />
+            <SelectIcon className="mr-3" as={ChevronDownIcon} />
+          </SelectTrigger>
+          <SelectPortal>
+            <SelectBackdrop/>
+            <SelectContent>
+              <SelectDragIndicatorWrapper>
+                <SelectDragIndicator />
+              </SelectDragIndicatorWrapper>
+              <SelectItem label="1" value="1" />
+              <SelectItem
+                label="2"
+                value="2"
+              />
+              <SelectItem
+                label="3"
+                value="3"
+                isDisabled={true}
+              />
+              <SelectItem
+                label="4"
+                value="4"
+              />
+            </SelectContent>
+          </SelectPortal>
+        </Select>
+    </div>
   );
 }
 
--- frontend/app/screens/Home.tsx
+++ frontend/app/screens/Home.tsx
@@ -7,8 +7,10 @@ import GreenButton from "../components/button";
 const Home = () => {
   return (
     <View>
-      <div className="flex flex-col gap-1 justify-center items-center"> 
+      <div className="flex flex-col gap-3 justify-center items-center p-6"> 
+        <h1 className="font-bold text-3xl p-2"> Upload Receipt </h1>
         <GreenButton />
+        <h1 className="font-bold text-3xl pt-16"> Current Ingredients </h1>
         <DropDownFilter />
       </div>
     </View>
