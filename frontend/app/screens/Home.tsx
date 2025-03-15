30a0ce01059af85d792856a17f341c47436fad8e
camellionn <thanhtra2004@gmail.com>
2025-03-16 01:28:58 +1100

    add in button component (solid positive variant)

--- frontend/app/screens/Home.tsx
+++ frontend/app/screens/Home.tsx
@@ -2,12 +2,14 @@ import { Heading } from "@/components/ui/heading";
 import React from "react";
 import { View } from "react-native";
 import DropDownFilter  from "../components/dropDownFilter"
+import GreenButton from "../components/button";
 
 const Home = () => {
   return (
     <View>
       <Heading>Home page</Heading>
-      <DropDownFilter />
+      <GreenButton />
+      <DropDownFilter /> 
     </View>
   );
 };
