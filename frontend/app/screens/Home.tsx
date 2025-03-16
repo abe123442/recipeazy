14ec41ba737f51567b1223d03cb20e4e2fe70282
Ahmed <ahmed.aden14@icloud.com>
2025-03-16 15:20:53 +1100

    move button to endpage

--- frontend/app/screens/Home.tsx
+++ frontend/app/screens/Home.tsx
@@ -8,13 +8,13 @@ const Home = () => {
   return (
     <View>
       <View className="flex flex-col gap-3 justify-center items-center p-6">
-        <Heading className="font-bold text-3xl p-2">Upload Receipt</Heading>
-        <ModalComponent />
         <Heading className="font-bold text-3xl pt-16">
           {' '}
           Current Ingredients{' '}
         </Heading>
         <DropDownFilter />
+        <Heading className="font-bold text-3xl p-2">Upload Receipt</Heading>
+        <ModalComponent />
       </View>
     </View>
   );
